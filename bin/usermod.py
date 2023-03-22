d415e3bb43cbc23723c7a41863ee11dfaf858423
Marie Antoinette <tetex7@outlook.com>
2023-03-22 06:48:32 -0400

    added more to usermod

--- bin/usermod.py
+++ bin/usermod.py
@@ -1,6 +1,22 @@
 from libdiscord.TP import DIS_TP
+from libdiscord.users import user
 #for lib.libdiscord
 import sys
+
+if (sys.argv[2] == "--help"):
+    t = DIS_TP(sys.argv[1])
+    t.ENBED(TITLE="TODO", TEXT="TODO")
+
+
+def dmain(FIT:str, ID:int, g:str):
+    t = DIS_TP(FIT)
+    u = user(ID)
+    if ((g == "") or (g == None)):
+        t.TEXT("NO TEXT ARG")
+        return
+    u.ADD_TO_GROUP(g)
+    t.TEXT("GROUP ADDED")
+    return
 
 def main(FIT:str, SR:str) -> int:
     
@@ -13,4 +29,7 @@ def main(FIT:str, SR:str) -> int:
     return
 
 
-main(sys.argv[1], sys.argv[2])
+try:
+    dmain(sys.argv[1], int(sys.argv[2]), sys.argv[3])
+except:
+    main(sys.argv[1], sys.argv[2])
